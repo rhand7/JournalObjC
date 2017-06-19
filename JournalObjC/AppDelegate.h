2b74096aefacbbfe927cf12a45d072c0cdd074e7
rhand7 <handje@BC-MacBookPro-0587d1.local>
2017-06-19 14:18:42 -0600

    Initialize repository

---
+++ JournalObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JournalObjC
+//
+//  Created by handje on 6/19/17.
+//  Copyright © 2017 Rob Hand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
